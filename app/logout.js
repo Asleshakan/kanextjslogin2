a7d31a7733d00985a2fa7167cfe92d1661815c1d
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-10-10 11:19:53 +0530

    Update logout.js

--- app/logout.js
+++ app/logout.js
@@ -1,3 +1,3 @@
 export default function Logout() {
-    return <a href="/api/auth/logout">Logout</a>;
+    return <a href="/api/auth/logout?returnTo=https://calm-stone-0060af810.5.azurestaticapps.net">Logout</a>;
   }
