81eb454875dee127b3a37da2d17a1495622c16c9
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-10-10 18:17:02 +0530

    Update route.js

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,12 +1,3 @@
-import { handleAuth, handleLogout } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth({
-  async logout(req, res) {
-    // Clear appSession cookie
-    res.setHeader('Set-Cookie', 'appSession=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=None; Secure');
-    
-    await handleLogout(req, res, {
-      returnTo: process.env.AUTH0_BASE_URL
-    });
-  }
-});
+export const GET = handleAuth();
