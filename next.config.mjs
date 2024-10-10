d51e10771b99399e0185f132172afdb882890be6
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-10-10 10:39:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    publicRuntimeConfig: {
+      auth0: {
+        session: {
+          cookie: {
+            transient: true, // Ensures cookies are not persistent
+            httpOnly: true,
+            sameSite: 'Lax', // Ensures the cookie is not accessible in cross-site contexts
+            secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+          },
+        },
+      },
+    },
+  };
+  
+  export default nextConfig;
+  
