a0d59c7ee890f28c99e3e7aed3b508b6d88aeb13
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2025-04-30 07:08:15 +0530

    Update logout.js

--- app/api/auth/[auth0]/logout.js
+++ app/api/auth/[auth0]/logout.js
@@ -1,9 +1,21 @@
-// pages/api/auth/logout.js
-import { handleLogout } from '@auth0/nextjs-auth0';
+//pages/api/logout-oidc.ts
 
-export default async function logout(req, res) {
-  // Call handleLogout to clear session cookies
-  await handleLogout(req, res, {
-    returnTo: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI || '/', // Redirect after logout
-  });
+import { getSession, handleLogout } from '@auth0/nextjs-auth0'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    const session = await getSession(req, res)
+    console.log({ session })
+
+    const logoutUrl = `${process.env.AUTH0_ISSUER_BASE_URL}/oidc/logout?clientId=${process.env.AUTH0_CLIENT_ID}&logout_hint=${session?.user.sid}`
+
+    await fetch(logoutUrl, {
+      method: 'GET'
+    })
+
+    return res.status(200).json({ message: 'Logged out successfully' })
+  }
 }
+
+export default handler
